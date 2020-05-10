c675b59bfb1a723880d797c99011d6b36a41975e
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-05-10 08:43:12 +0200

    Allow include attachments in iframe

--- django_email_log/urls.py
+++ django_email_log/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.urls import re_path
 
 from .views import AttachmentView
 
 
 urlpatterns = [
-	re_path(r'^parts/(?P<object_type>(attachment|alternative))/(?P<pk>\d+)/(?P<nr>\d+)/', AttachmentView.as_view(), name='django_email_log_attachment'),
+	re_path(r'^parts/(?P<object_type>(attachment|alternative))/(?P<pk>\d+)/(?P<nr>\d+)/', xframe_options_sameorigin(AttachmentView.as_view()), name='django_email_log_attachment'),
 ]
